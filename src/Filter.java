05f496ed1ae84aa3f22b968a6bb8d4af06073f3d
A-Salty-Fish <1309032417@qq.com>
2022-01-12 10:12:37 +0800

    init java project

---
+++ src/Filter.java
@@ -0,0 +1,12 @@
+/**
+ * @author dzy
+ * @title: Filter
+ * @projectName textfilter
+ * @description: TODO
+ * @date 2022/1/12 10:11
+ */
+public class Filter {
+    public static void main(String[] args) {
+        System.out.println(" ");
+    }
+}
